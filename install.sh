7e233952d2f0a81d36c5e22a68293d7bd648f50c
PandeCode <pandeshawnbenjamin@gmail.com>
2021-09-06 15:52:29 +0300

    refresh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+~/dotfiles/setup/systemFilesSetup.sh
+~/dotfiles/setup/getTheme.sh
+~/dotfiles/setup/tmuxSetup.sh
+~/dotfiles/setup/configSetup.sh
--- runAllScripts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-~/dotfiles/setupScripts/systemFilesSetup.sh
-~/dotfiles/setupScripts/getTheme.sh
-~/dotfiles/setupScripts/tmuxSetup.sh
-~/dotfiles/setupScripts/configSetup.sh
