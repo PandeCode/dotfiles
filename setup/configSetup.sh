f9e1df947b902444878688bba45535ba294e255d
PandeCode <pandeshawnbenjamin@gmail.com>
2021-10-15 10:56:02 +0300

    rustfmt

--- setup/configSetup.sh
+++ setup/configSetup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 rm -fr ~/.config/clangd \
-        ~/.config/bat \
-        ~/.config/kitty \
-        ~/.config/eww \
-        ~/.config/dunst \
-        ~/.config/fish \
-        ~/.config/fontconfig \
-        ~/.config/gtk-2.0 \
-        ~/.config/gtk-3.0 \
-        ~/.config/gtk-4.0 \
-        ~/.config/i3 \
-        ~/.config/i3blocks \
-        ~/.config/nvim \
-        ~/.config/picom \
-        ~/.config/rofi \
-        ~/.config/ranger
+	~/.config/bat \
+	~/.config/dunst \
+	~/.config/eww \
+	~/.config/fish \
+	~/.config/fontconfig \
+	~/.config/gtk-2.0 \
+	~/.config/gtk-3.0 \
+	~/.config/gtk-4.0 \
+	~/.config/i3 \
+	~/.config/i3blocks \
+	~/.config/kitty \
+	~/.config/nvim \
+	~/.config/picom \
+	~/.config/ranger \
+	~/.config/rofi \
+	~/.config/rustfmt
 
-ln -s ~/dotfiles/config/kitty ~/.config/kitty
 ln -s ~/dotfiles/config/bat ~/.config/bat
-ln -s ~/dotfiles/config/eww ~/.config/eww
-ln -s ~/dotfiles/config/dunst ~/.config/dunst
 ln -s ~/dotfiles/config/clangd ~/.config/clangd
+ln -s ~/dotfiles/config/dunst ~/.config/dunst
+ln -s ~/dotfiles/config/eww ~/.config/eww
 ln -s ~/dotfiles/config/fish ~/.config/fish
 ln -s ~/dotfiles/config/fontconfig ~/.config/fontconfig
 ln -s ~/dotfiles/config/gtk-2.0 ~/.config/gtk-2.0
@@ -29,7 +29,9 @@ ln -s ~/dotfiles/config/gtk-3.0 ~/.config/gtk-3.0
 ln -s ~/dotfiles/config/gtk-4.0 ~/.config/gtk-4.0
 ln -s ~/dotfiles/config/i3 ~/.config/i3
 ln -s ~/dotfiles/config/i3blocks ~/.config/i3blocks
+ln -s ~/dotfiles/config/kitty ~/.config/kitty
 ln -s ~/dotfiles/config/nvim ~/.config/nvim
 ln -s ~/dotfiles/config/picom ~/.config/picom
-ln -s ~/dotfiles/config/rofi ~/.config/rofi
 ln -s ~/dotfiles/config/ranger ~/.config/ranger
+ln -s ~/dotfiles/config/rofi ~/.config/rofi
+ln -s ~/dotfiles/config/rustfmt ~/.config/rustfmt
