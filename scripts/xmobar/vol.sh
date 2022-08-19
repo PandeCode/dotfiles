db24728d16a98b0dd1eb7af996178d152cf655b7
PandeCode <pandeshawnbenjamin@gmail.com>
2022-08-19 14:56:08 +0300

    Vol

--- scripts/xmobar/vol.sh
+++ scripts/xmobar/vol.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 getVol() {
-	amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }'
+	pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\/\s+\K\d+%(?=\s+\/)' | head -n 1
 }
 
 isMuted() {
@@ -44,14 +44,20 @@ EOF
 		)"
 
 		$cmd
-
+		exit 0
 		;;
 
 	4)
 		volUp
+
+		exit 0
 		;;
 
 	5)
 		volDown
+
+		exit 0
 		;;
 esac
+
+echo -n "<action=\`~/dotfiles/scripts/xmobar/vol.sh 1\` button=1><action=\`~/dotfiles/scripts/xmobar/vol.sh 2\` button=2><action=\`~/dotfiles/scripts/xmobar/vol.sh 3\` button=3><action=\`~/dotfiles/scripts/xmobar/vol.sh 4\` button=4><action=\`~/dotfiles/scripts/xmobar/vol.sh 5\` button=5>$(getVol) </action></action></action></action></action>"
