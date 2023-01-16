8ee5a317b6c92499936e71aeae4f2da67356b2f2
PandeCode <pandeshawnbenjamin@gmail.com>
2023-01-16 18:13:01 +0300

    Hyprland

--- setup/configSetup.sh
+++ setup/configSetup.sh
@@ -9,6 +9,7 @@ rm -fr ~/.config/bat \
 	~/.config/gtk-2.0 \
 	~/.config/gtk-3.0 \
 	~/.config/gtk-4.0 \
+	~/.config/hypr \
 	~/.config/i3 \
 	~/.config/i3blocks \
 	~/.config/kitty \
@@ -31,6 +32,7 @@ ln -s ~/dotfiles/config/fontconfig ~/.config/fontconfig
 ln -s ~/dotfiles/config/gtk-2.0 ~/.config/gtk-2.0
 ln -s ~/dotfiles/config/gtk-3.0 ~/.config/gtk-3.0
 ln -s ~/dotfiles/config/gtk-4.0 ~/.config/gtk-4.0
+ln -s ~/dotfiles/config/hypr ~/.config/hypr
 ln -s ~/dotfiles/config/i3 ~/.config/i3
 ln -s ~/dotfiles/config/i3blocks ~/.config/i3blocks
 ln -s ~/dotfiles/config/kitty ~/.config/kitty
