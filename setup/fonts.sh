c5bbde25b6c1fef32736b42a4282c82e8a300f9f
PandeCode <pandeshawnbenjamin@gmail.com>
2023-02-10 14:28:34 +0300

    fonts.sh

--- setup/fonts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/.fonts/
-cd ~/.fonts/ || return 1
-wget -c $(curl -fsSL https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq -r '.assets[] | .browser_download_url' | grep FantasqueSansMono.zip)
-unzip FantasqueSansMono.zip
